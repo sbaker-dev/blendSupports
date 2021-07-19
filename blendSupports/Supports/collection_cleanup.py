7f96b4b99df30f6ad3c7608e3035da09d4dd6e2f
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-07-19 14:43:47 +0100

    Allow for cameras to be deleted, make it a general camera validation on class type rather than name, and permit the deletion of curve objects which are not meshes

--- blendSupports/Supports/collection_cleanup.py
+++ blendSupports/Supports/collection_cleanup.py
@@ -1,13 +1,18 @@
+import bpy_types
 import bpy
 
 
-def collection_cleanup(collection_name):
+def collection_cleanup(collection_name, save_camera=True):
     """
     Cleanup a collection of objects no longer required
 
     Source: https://blender.stackexchange.com/questions/153713/removing-all-meshes-from-a-collection-with-python
+          : https://blender.stackexchange.com/questions/32349/python-scripting-remove-curve-object
 
     :param collection_name: Name of the collection iterate through
+
+    :param save_camera: If True then this process will exclude cameras in this collection, defaults to True.
+
     :return: Nothing, delete the objects requested then stop
     """
     # Cleanup the collection
@@ -16,10 +21,28 @@ def collection_cleanup(collection_name):
     meshes = set()
     for obj in collection.objects:
         meshes.add(obj.data)
-        if obj.name != "Camera":
+
+        if save_camera:
+            if not isinstance(obj.data, bpy.types.Camera):
+                bpy.data.objects.remove(obj)
+        else:
             bpy.data.objects.remove(obj)
 
-    # Look at meshes that are orphean after objects removal
+    # Look at meshes that are orphaned after objects removal
     for mesh in [m for m in meshes if m.users == 0]:
-        # Delete the meshes
-        bpy.data.meshes.remove(mesh)
+
+        # Delete meshes
+        if isinstance(mesh, bpy_types.Mesh):
+            bpy.data.meshes.remove(mesh)
+
+        # Delete Curves
+        elif isinstance(mesh, bpy.types.TextCurve):
+            bpy.data.curves.remove(mesh)
+
+        # Delete Camera
+        elif isinstance(mesh, bpy.types.Camera):
+            bpy.data.cameras.remove(mesh)
+
+        # Warn users if we find mesh type we have not handled
+        else:
+            print("Unknown mesh type: cannot delete")
