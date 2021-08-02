6421cb706c054eee5d0ed477e7964e2e4daa3ece
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-08-02 15:33:42 +0100

    Exposue Camera name so it can be a different camera, genreralise setup camera

--- blendSupports/Renders/render.py
+++ blendSupports/Renders/render.py
@@ -17,12 +17,26 @@ class CameraTypeError(Exception):
 
 def render_scene(camera_position, write_directory, write_name, engine="CYCLES", x_resolution=1080,
                  y_resolution=1080, camera_type="ORTHO", camera_scale=5, save_file=True, film_transparent=True,
-                 file_type="png"):
+                 file_type="png", camera_name="Camera"):
 
     if engine.upper() not in ["CYCLES", "BLENDER_EEVEE"]:
         raise EngineInvalidError(engine.upper())
     bpy.context.scene.render.engine = engine
 
+    # Setup the camera
+    setup_camera(camera_name, camera_position, camera_scale, camera_type, film_transparent, x_resolution, y_resolution)
+
+    # Render the scene
+    bpy.context.scene.render.filepath = str(Path(write_directory, f"{write_name}.{file_type}").absolute())
+    bpy.context.scene.eevee.use_gtao = True
+    bpy.ops.render.render(write_still=True)
+
+    if save_file:
+        # Save the blend file for manual manipulation later
+        bpy.ops.wm.save_as_mainfile(filepath=f"{write_directory}/{write_name}.blend")
+
+
+def setup_camera(camera_name, camera_position, camera_scale, camera_type, film_transparent, x_resolution, y_resolution):
     # Set the output resolution and camera scale
     bpy.context.scene.render.resolution_x = int(x_resolution)
     bpy.context.scene.render.resolution_y = int(y_resolution)
@@ -31,18 +45,19 @@ def render_scene(camera_position, write_directory, write_name, engine="CYCLES", 
     if camera_type.upper() not in ['PANO', "ORTHO", "PERSP"]:
         raise CameraTypeError(camera_type.upper())
 
-    camera = bpy.data.objects["Camera"]
+    # Find Camera, set camera type
+    camera = bpy.data.objects[camera_name]
     camera.data.type = camera_type.upper()
+
+    # Set the cameras location, and if camera type is orthographic set orthographic scale
     camera.location = tuple_convert(camera_position)
     if camera_type == "ORTHO":
         camera.data.ortho_scale = float(camera_scale)
 
-    # Render the scene
-    bpy.context.scene.render.filepath = str(Path(write_directory, f"{write_name}.{file_type}").absolute())
-    bpy.context.scene.eevee.use_gtao = True
+    # Set transparency
     bpy.context.scene.render.film_transparent = film_transparent
-    bpy.ops.render.render(write_still=True)
 
-    if save_file:
-        # Save the blend file for manual manipulation later
-        bpy.ops.wm.save_as_mainfile(filepath=f"{write_directory}/{write_name}.blend")
+
+
+def open_gl_render():
+    return
